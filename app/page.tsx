79d78efc6349f3145e6a0ab4b5d48b873e00da97
VasO <dev.dyger@gmail.com>
2023-11-09 20:06:00 +0100

    feat(music card): add more png

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-12">
                 <MusicCard name={"lofi"}/>
                 <MusicCard name={"piano"}/>
-                <MusicCard name={"piano"}/>
-                <MusicCard name={"lofi"}/>
+                <MusicCard name={"handpan"}/>
+                <MusicCard name={"jazz"}/>
 
             </div>
             <CardContainer>
--- components/MusicCard.tsx
+++ components/MusicCard.tsx
@@ -7,16 +7,20 @@ interface NoiseCardProps {
 const MusicCard = ({name}: NoiseCardProps) => {
     return (
         <a
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className="h-56 w-56 flex flex-col justify-between group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
-            <Image
-                src={`/images/${name}.png`}
-                width={175}
-                height={175}
-                alt="The lofi music logo"
-            />
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-center`}>
-                LOFI
+            <div style={{position: 'relative', width: '180px', height: '180px'}}>
+                <Image
+                    src={`/images/${name}.png`}
+                    fill={true}
+                    alt="The lofi music logo"
+                    style={{
+                        objectFit: 'contain',
+                    }}
+                />
+            </div>
+            <p className={`mt-2 max-w-[30ch] text-sm opacity-50 text-center`}>
+                {name.toUpperCase()}
             </p>
         </a>
     )
