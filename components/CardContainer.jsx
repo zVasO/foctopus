1f39359252ef1eee296c4b5a8393d6e9e682a181
VasO <dev.dyger@gmail.com>
2023-11-09 19:02:15 +0100

    feat(main page): create some components

---
+++ components/CardContainer.jsx
@@ -0,0 +1,7 @@
+export default function CardContainer({children}) {
+    return (
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            {children}
+        </div>
+    )
+}
