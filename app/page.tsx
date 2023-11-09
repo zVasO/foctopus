5b8221b47c54390ed594a1abfcc93302a39971be
VasO <dev.dyger@gmail.com>
2023-11-09 19:19:59 +0100

    feat(main page): change the way to declare component, put file in tsx, add piano png, call the name of the music for get the logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,35 @@
-
 import React from "react";
 import MusicCard from "@/components/MusicCard";
 import CardContainer from "@/components/CardContainer";
 import NoiseCard from "@/components/NoiseCard";
 
 export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-12">
-          <MusicCard/>
-          <MusicCard/>
-          <MusicCard/>
-          <MusicCard/>
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-12">
+                <MusicCard name={"lofi"}/>
+                <MusicCard name={"piano"}/>
+                <MusicCard name={"piano"}/>
+                <MusicCard name={"lofi"}/>
 
-        </div>
-        <CardContainer>
-          <NoiseCard/>
-          <NoiseCard/>
-          <NoiseCard/>
-          <NoiseCard/>
+            </div>
+            <CardContainer>
+                <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
 
-          <NoiseCard/>
-          <NoiseCard/>
-          <NoiseCard/>
-          <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
 
-          <NoiseCard/>
-          <NoiseCard/>
-          <NoiseCard/>
-          <NoiseCard/>
-        </CardContainer>
-      </main>
+                <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
+                <NoiseCard/>
+            </CardContainer>
+        </main>
 
-  )
+    )
 }
---
+++ components/CardContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface CardContainerProps {
+    children: React.ReactNode[];
+}
+
+const CardContainer: React.FC<CardContainerProps> = ({children}) => {
+    return (
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            {children}
+        </div>
+    )
+}
+export default CardContainer;
