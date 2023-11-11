b06819f7bec433511d56bf007148144a450a3bc6
DylanGermann <dev.dyger@gmail.com>
2023-11-11 12:24:08 +0100

    feat(mui): add mui library and set a custom theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import MusicCard from "@/components/MusicCard";
 import CardContainer from "@/components/CardContainer";
@@ -5,34 +7,37 @@ import NoiseCard from "@/components/NoiseCard";
 import '../public/css/jtbIL.css'
 import '../public/css/jtbI.css'
 import '../public/css/jtbIB.css'
+import {ThemeProvider} from "@mui/material";
+import theme from "@/app/theme";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-12">
-                <MusicCard name={"lofi"}/>
-                <MusicCard name={"piano"}/>
-                <MusicCard name={"handpan"}/>
-                <MusicCard name={"jazz"}/>
+        <ThemeProvider theme={theme}>
+            <main className="flex min-h-screen flex-col items-center p-24">
+                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-12">
+                    <MusicCard name={"lofi"}/>
+                    <MusicCard name={"piano"}/>
+                    <MusicCard name={"handpan"}/>
+                    <MusicCard name={"jazz"}/>
 
-            </div>
-            <CardContainer>
-                <NoiseCard svgName={'jtbIL-cloud-hail-rain'} name={'rain'}/>
-                <NoiseCard svgName={'jtbIL-campfire'} name={'campfire'}/>
-                <NoiseCard svgName={'jtbIL-wind'} name={'wind'}/>
-                <NoiseCard svgName={'jtbIL-book'} name={'book page'}/>
+                </div>
+                <CardContainer>
+                    <NoiseCard svgName={'jtbIL-cloud-hail-rain'} name={'rain'}/>
+                    <NoiseCard svgName={'jtbIL-campfire'} name={'campfire'}/>
+                    <NoiseCard svgName={'jtbIL-wind'} name={'wind'}/>
+                    <NoiseCard svgName={'jtbIL-book'} name={'book page'}/>
 
-                <NoiseCard svgName={'jtbIL-cloud-bolt'} name={'thunder'}/>
-                <NoiseCard svgName={'jtbIL-flood'} name={'flood'}/>
-                <NoiseCard svgName={'jtbIL-typewriter'} name={'keyboard'}/>
-                <NoiseCard svgName={'jtbIL-cloud-hail-rain'} name={'rain'}/>
+                    <NoiseCard svgName={'jtbIL-cloud-bolt'} name={'thunder'}/>
+                    <NoiseCard svgName={'jtbIL-flood'} name={'flood'}/>
+                    <NoiseCard svgName={'jtbIL-typewriter'} name={'keyboard'}/>
+                    <NoiseCard svgName={'jtbIL-cloud-hail-rain'} name={'rain'}/>
 
-                <NoiseCard svgName={'jtbIL-cloud-hail-rain'} name={'rain'}/>
-                <NoiseCard svgName={'jtbIL-campfire'} name={'campfire'}/>
-                <NoiseCard svgName={'jtbIL-wind'} name={'wind'}/>
-                <NoiseCard svgName={'jtbIL-book'} name={'book page'}/>
-            </CardContainer>
-        </main>
-
+                    <NoiseCard svgName={'jtbIL-cloud-hail-rain'} name={'rain'}/>
+                    <NoiseCard svgName={'jtbIL-campfire'} name={'campfire'}/>
+                    <NoiseCard svgName={'jtbIL-wind'} name={'wind'}/>
+                    <NoiseCard svgName={'jtbIL-book'} name={'book page'}/>
+                </CardContainer>
+            </main>
+        </ThemeProvider>
     )
 }
---
+++ app/theme.tsx
@@ -0,0 +1,14 @@
+import {createTheme} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000000',
+        },
+        secondary: {
+            main: '#FFFFFF',
+        },
+    },
+});
+
+export default theme;
