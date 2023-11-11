e321e9d00d573fd28e29e57d06ca987b40f1b185
DylanGermann <dev.dyger@gmail.com>
2023-11-11 20:47:20 +0100

    feat(sound player): create a sound player with howl dependency

---
+++ components/SoundPlayer.tsx
@@ -0,0 +1,53 @@
+import {useEffect, useRef, useState} from "react";
+import {Howl} from 'howler';
+
+interface SoundPlayerProps {
+    soundPath: string;
+    isActive: boolean;
+    volume?: number;
+}
+
+const SoundPlayer = ({soundPath, isActive, volume}: SoundPlayerProps) => {
+    const soundRef = useRef<Howl | null>(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        if (!soundPath) {
+            console.error('No sound path provided.');
+            return;
+        }
+
+        if (!soundRef.current) {
+            soundRef.current = new Howl({
+                src: [soundPath],
+                volume: 0.5,
+                loop: true,
+                html5: true
+            });
+        }
+
+        const sound = soundRef.current;
+
+        if (volume !== undefined) {
+            const adjustedVolume = Math.max(0, Math.min(volume / 100, 1));
+            sound.volume(adjustedVolume);
+            console.log('volume', adjustedVolume);
+        }
+
+        if (isActive && !isPlaying) {
+            sound.play();
+            setIsPlaying(true);
+        } else if (!isActive && isPlaying) {
+            sound.stop();
+            setIsPlaying(false);
+        }
+
+        return () => {
+            // Do not unload the sound to keep it persistent
+        };
+    }, [soundPath, isActive, volume, isPlaying]);
+
+    return null;
+};
+
+export default SoundPlayer;
